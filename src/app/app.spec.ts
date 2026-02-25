3261de607dc70eb06b4c5b4f63b889bf4f3eb491
dnyvelasquez <dny.velasquez@gmail.com>
2026-02-25 14:20:33 -0500

    fifa fix

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NFL-Predictor');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FIFA-Predictor');
   });
 });
